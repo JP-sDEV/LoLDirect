37b254fd56bfa76d38361ef8999c88c988fa0ac6
JP-sDEV <jon.pablo80@gmail.com>
2020-02-14 14:52:43 -0500

    fixed MERGE issues

--- client/src/Pages/NavBar.js
+++ client/src/Pages/NavBar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { AppContext, AppProvider } from "../AppContext";
@@ -6,13 +5,6 @@ import { SearchPlayers } from "../components/SearchPlayers";
 import { BrowserRouter as Router, Switch, useLocation } from "react-router-dom";
 export const NavBar = () => {
   const location = useLocation();
-=======
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AppContext, AppProvider } from "../AppContext";
-import { SearchPlayers } from "../components/SearchPlayers";
-export const NavBar = () => {
->>>>>>> d1e8c74b99f1dcbb9ce377360c97a63d34983d3e
   return (
     <section className="grid" id="showCase">
       <div id="headerTitle">
@@ -26,16 +18,12 @@ export const NavBar = () => {
           src="https://cdn2.downdetector.com/static/uploads/logo/image1.png"
         />
       </div>
-<<<<<<< HEAD
       {location.pathname !== "/MakeATeam" && <span></span> && (
         <Fragment>
           <SearchPlayers />
         </Fragment>
       )}
       {/* <AppContext.Consumer>{context => <SearchPlayers />}</AppContext.Consumer> */}
-=======
-      <AppContext.Consumer>{context => <SearchPlayers />}</AppContext.Consumer>
->>>>>>> d1e8c74b99f1dcbb9ce377360c97a63d34983d3e
       <ul className="navBar">
         <Link className="navLink" to="/">
           Home
